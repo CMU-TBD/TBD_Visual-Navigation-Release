0b9e28c088c0496cb19f94f3504beea3ca86eb1e
GustavoSilvera <gustavo.r.silvera@gmail.com>
2020-06-05 10:53:07 -0700

    looping through states to generate piecewise spline

--- tests/test_cost_function.py
+++ tests/test_cost_function.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import copy
 import tensorflow as tf
 tf.enable_eager_execution()
 from obstacles.sbpd_map import SBPDMap
@@ -122,53 +123,53 @@ def test_cost_function(plot=False):
     dt = .01 # delta-t: time intervals
     k = 100  # Number of time-steps (should be 1/dt)
 
-    target_state = (18, 16.5, np.pi/2.0, 0.2)   # Start State (x, y, theta, vel)
-    middle_state = (15, 14.5, np.pi/2.0, 0.8)   # Middle State (x, y, theta, vel)
-    start_state = (8, 12, np.pi/2.0, 0.5)       # Goal State (x, y, theta, vel)
-
-    # Generate start position
-    start_posx_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * start_state[0]
-    start_posy_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * start_state[1]
-    start_pos_nk2 = tf.concat([start_posx_nk1, start_posy_nk1], axis=2)
-    # Generate start speed and heading
-    start_heading_nk1 = tf.ones((n, 1, 1), dtype=tf.float32)*start_state[2]
-    start_speed_nk1 = tf.ones((n, 1, 1), dtype=tf.float32)*start_state[3]
-    
-    # Generate middle position
-    middle_posx_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * middle_state[0]
-    middle_posy_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * middle_state[1]
-    middle_pos_nk2 = tf.concat([middle_posx_nk1, middle_posy_nk1], axis=2)
-    # Generate middle speed and heading
-    middle_heading_nk1 = tf.ones((n, 1, 1), dtype=tf.float32)*middle_state[2]
-    middle_speed_nk1 = tf.ones((n, 1, 1), dtype=tf.float32)*middle_state[3]
-    
-    # Generate goal position
-    goal_posx_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * target_state[0]
-    goal_posy_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * target_state[1]
-    goal_pos_nk2 = tf.concat([goal_posx_nk1, goal_posy_nk1], axis=2)
-    # Generate goal speed and heading
-    goal_heading_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * target_state[2]
-    goal_speed_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * target_state[3]
-
-    start_config = SystemConfig(dt, n, 1, position_nk2=start_pos_nk2, speed_nk1=start_speed_nk1, heading_nk1=start_heading_nk1, variable=False)
-    middle_config = SystemConfig(dt, n, 1, position_nk2=middle_pos_nk2, speed_nk1=middle_speed_nk1, heading_nk1=middle_heading_nk1, variable=False)
-    goal_config = SystemConfig(dt, n, 1, position_nk2=goal_pos_nk2, speed_nk1=goal_speed_nk1, heading_nk1=goal_heading_nk1, variable=True)
+    # States represents each individual tangent point that the spline will pass through
+    # states[0] = initial state, and states[len(states) - 1] = terminal state
+    states = [
+        (8, 12, np.pi/2.0, 0.5),    # Start State (x, y, theta, vel)
+        (15, 14.5, np.pi/2.0, 0.8), # Middle State (x, y, theta, vel)
+        (10, 15, -np.pi/2.0, 1),    # Middle State (x, y, theta, vel)
+        (18, 16.5, np.pi/2.0, 0.2)  # Goal State (x, y, theta, vel)
+        ]
 
     p = DotMap(spline_params=DotMap(epsilon=1e-5))
     ts_nk = tf.tile(tf.linspace(0., dt*k, k)[None], [n, 1])
-    spline_traj = Spline3rdOrder(dt=dt, k=k, n=n, params=p.spline_params)
-    spline_traj.fit(start_config, middle_config, factors=None)
-    spline_traj.eval_spline(ts_nk, calculate_speeds=True)
-    
-    spline_traj2 = Spline3rdOrder(dt=dt, k=k, n=n, params=p.spline_params)
-    spline_traj2.fit(middle_config, goal_config, factors=None)
-    spline_traj2.eval_spline(ts_nk, calculate_speeds=True)
+    splines = []
+    prev_config = None
+    next_config = None
+    # Generate all two-point splines from the states
+    for s in states:
+        spline_traj = Spline3rdOrder(dt=dt, k=k, n=n, params=p.spline_params)
+        # Keep track of old trajectory
+        if(next_config is not None):
+            prev_config = copy.deepcopy(next_config)
+        # Generate position
+        posx_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * s[0]
+        posy_nk1 = tf.ones((n, 1, 1), dtype=tf.float32) * s[1]
+        pos_nk2 = tf.concat([posx_nk1, posy_nk1], axis=2)
+        # Generate speed and heading
+        heading_nk1 = tf.ones((n, 1, 1), dtype=tf.float32)*s[2]
+        speed_nk1 = tf.ones((n, 1, 1), dtype=tf.float32)*s[3]
+        next_config = SystemConfig(dt, n, 1, position_nk2=pos_nk2, speed_nk1=speed_nk1, heading_nk1=heading_nk1, variable=False)
 
-    spline_traj.append_along_time_axis(spline_traj2)
+        # Append to the trajectory if there is a past version
+        if(prev_config is not None):
+            spline_traj.fit(prev_config, next_config, factors=None)
+            spline_traj.eval_spline(ts_nk, calculate_speeds=True)
+            splines.append(copy.deepcopy(spline_traj))
+    
+    # Loop through all calculated splines to combine them together into a singular one
+    final_spline = None
+    for i, s in enumerate(splines):
+        if(final_spline is not None):
+            final_spline.append_along_time_axis(s)
+        if(i == 0):
+            # For first spline
+            final_spline = copy.deepcopy(s)
 
     fig = plt.figure()
     fig, ax = plt.subplots(4,1, figsize=(5,15), squeeze=False)
-    spline_traj.render(ax, freq=4, plot_heading=True, plot_velocity=True, label_start_and_end=True)
+    final_spline.render(ax, freq=4, plot_heading=True, plot_velocity=True, label_start_and_end=True)
     # trajectory.render(ax, freq=1, plot_heading=True, plot_velocity=True, label_start_and_end=True)
     fig.savefig('./tests/cost/trajectory.png', bbox_inches='tight', pad_inches=0)
 
